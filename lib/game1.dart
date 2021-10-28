09e3c6ddf969e36e99b1fa60325395e50d4b094e
malak4822 <kmalak4822@gmail.com>
2021-10-28 11:31:36 +0200

    commit

--- lib/game1.dart
+++ lib/game1.dart
@@ -1,5 +1,4 @@
 import 'package:flany/game2.dart';
-import 'package:flany/informations.dart';
 import 'package:flany/res1.dart';
 import 'package:flany/results.dart';
 import 'package:flutter/material.dart';
